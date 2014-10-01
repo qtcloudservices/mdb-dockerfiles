a7ce1a4a90eb9b0a37799e13cc59d48c736f5b60
Jari Kolehmainen <jari.kolehmainen@digia.com>
2014-10-01 07:31:29 +0300

    use erb to generate mysql config

--- mysql/5.6/configure_mysql.sh
+++ mysql/5.6/configure_mysql.sh
@@ -3,9 +3,5 @@
 if [ -f "/etc/mysql/conf.d/mysqld_safe_syslog.cnf" ]; then
   rm "/etc/mysql/conf.d/mysqld_safe_syslog.cnf"
 fi
-
-if [ -f "/etc/mysql/my-$MDB_INSTANCE_SIZE.cnf" ]; then
-  cp "/etc/mysql/my-$MDB_INSTANCE_SIZE.cnf" /etc/mysql/conf.d/mdb.cnf
-else
-  cp "/etc/mysql/my-1.cnf" /etc/mysql/conf.d/mdb.cnf
-fi
+export MDB_SIZE=${MDB_SIZE:-1}
+erb "/etc/mysql/my.cnf.erb" > /etc/mysql/conf.d/mdb.cnf
